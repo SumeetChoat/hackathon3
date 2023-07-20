bbb6312c269e01ba577c1be260adde95da6868f7
mahirjali <mahirjalil@hotmail.com>
2023-07-20 17:15:55 +0100

    Added image

--- hackathon/src/pages/PokeInfoPage/index.jsx
+++ hackathon/src/pages/PokeInfoPage/index.jsx
@@ -18,6 +18,7 @@ export default function PokeInfoPage() {
 
     return (
       <div>
+        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}/>
           <h2>Name: {show.name}</h2>
           <h2>Height: {show.height}</h2>
           <h2>Weight: {show.weight}</h2>
